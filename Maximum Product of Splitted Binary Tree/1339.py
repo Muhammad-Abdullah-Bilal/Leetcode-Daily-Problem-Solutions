7676df056abdb3898a9168fd00e3fe753fcc2fc5
Muhammad-Abdullah-Bilal <abdullahbilal332333@gmail.com>
2026-01-19 09:59:03 +0500

    Added solution of maximum product of splitted binary tree problem

---
+++ Maximum Product of Splitted Binary Tree/1339.py
@@ -0,0 +1,37 @@
+from typing import Optional
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def maxProduct(self, root: Optional[TreeNode]) -> int:
+        MOD = 10**9 + 7
+        self.max_product = 0
+
+        def totalSum(node):
+            if not node:
+                return 0
+            return node.val + totalSum(node.left) + totalSum(node.right)
+
+        total = totalSum(root)
+
+        def dfs(node):
+            if not node:
+                return 0
+
+            left = dfs(node.left)
+            right = dfs(node.right)
+
+            subtree_sum = node.val + left + right
+
+            product = subtree_sum * (total - subtree_sum)
+            self.max_product = max(self.max_product, product)
+
+            return subtree_sum
+
+        dfs(root)
+        return self.max_product % MOD
