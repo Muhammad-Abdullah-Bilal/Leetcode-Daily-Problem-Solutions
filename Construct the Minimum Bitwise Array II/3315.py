4680d5721881010bfbb3fb7a6cf43efab2b26aaf
Muhammad-Abdullah-Bilal <abdullahbilal332333@gmail.com>
2026-02-02 10:06:16 +0500

    Added solution of construct the minimum bitwise array II problem

---
+++ Construct the Minimum Bitwise Array II/3315.py
@@ -0,0 +1,19 @@
+from typing import List
+class Solution:
+    def minBitwiseArray(self, nums: List[int]) -> List[int]:
+        ans = []
+
+        for num in nums:
+            if num % 2 == 0:
+                ans.append(-1)
+                continue
+
+            k = 0
+            temp = num
+            while temp & 1:
+                k += 1
+                temp >>= 1
+
+            ans.append(num - (1 << (k - 1)))
+
+        return ans
